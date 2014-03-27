f77d2f1766296ab658536d130c57e8e7c7d68286
Christopher Leonard <veltas.fakeemail@server.fake>
2014-03-27 18:07:11 +0000

    Restructured build script.

--- build_all.cmd
+++ build_all.cmd
@@ -1,10 +1,17 @@
+:: TODO - add checks for existing files for make-style building (or write for
+:: an actual make utility, this is pretty ugly)
+
 @SETLOCAL
 
-@SET OUTPUTNAME=maze_game.exe
 @SET LIBINPUT=user32.lib kernel32.lib Gdiplus.lib Gdi32.lib
-@SET PRECOMPILED=
-@SET INPUTFILES=main.cpp wtotal_failure_error.c defs_n_consts.cpp game.cpp draw_game.cpp
 
-@CL /EHsc /Fe%OUTPUTNAME% %LIBINPUT% %PRECOMPILED% %INPUTFILES%
+@SET CLARGS=/EHsc /Ox
+
+@CL /c %CLARGS% /Fowtotal_failure_error.obj wtotal_failure_error.c
+@CL /c %CLARGS% /Fodefs_n_consts.obj defs_n_consts.cpp
+@CL /c %CLARGS% /Fogame.obj game.cpp
+@CL /c %CLARGS% /Fodraw_game.obj draw_game.cpp
+
+@CL %CLARGS% /Femaze_game.exe main.cpp wtotal_failure_error.obj defs_n_consts.obj game.obj draw_game.obj %LIBINPUT%
 
 @ENDLOCAL
